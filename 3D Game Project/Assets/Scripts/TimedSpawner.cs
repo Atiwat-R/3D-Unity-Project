2f651948e7710b9f8626be2e16d9ae7eda76f4d6
Atiwat-R <75544677+Atiwat-R@users.noreply.github.com>
2022-04-06 17:24:41 +0700

    Added TimedSpawner & Tank Spawner
    - Added generalized TimedSpawner script
    - Added Tank Spawner (Spawn 2 tanks every 20 seconds, with a limit of 15-17 tanks at a time)

---
+++ 3D Game Project/Assets/Scripts/TimedSpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedSpawner : MonoBehaviour
+{
+
+    public GameObject spawnee; // Specify what Prefab to spawn
+    private int maximumSpawnee = 15; // 
+    public bool stopSpawning = false;
+
+    [SerializeField] float spawnTime; // How long before we start the spawning
+    [SerializeField] float spawnDelay; // Delay between each
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Debug.Log(GameObject.FindGameObjectsWithTag("Enemy").Length);
+
+        // If enemy has decreased below limit, turn it on again. 
+        if (stopSpawning == true && GameObject.FindGameObjectsWithTag("Enemy").Length <= maximumSpawnee) {
+            InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+        }
+    }
+
+    void SpawnObject()
+    {
+        Instantiate(spawnee, transform.position, transform.rotation);
+
+        // If there's too many spawnee, turn off spawner.
+        if (GameObject.FindGameObjectsWithTag("Enemy").Length > maximumSpawnee) {
+            stopSpawning = true;
+            CancelInvoke("SpawnObject");
+        }
+    }
+}
